aa73c7b95dbfbd6f8ce0d3b6fb2b1ea35827bc9d
Timur <timurka.askarov@inbox.ru>
2025-04-27 00:09:16 +0300

    book page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css'
 import {} from "@/components/ui/navigation-menu"
 import LayoutHeader from "@/shared/LayoutHeader";
 import {Routes, Route, BrowserRouter, Navigate} from "react-router-dom";
-import HomePage from '@/pages/Home';
-import MyBooksPage from "@/pages/MyBooks";
+import HomePage from '@/pages/HomePage';
+import MyBooksPage from "@/pages/MyBooksPage";
 import ProfilePage from "@/pages/ProfilePage";
+import BookPage from "@/pages/BookPage";
 
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
                         <Route path="/my-books" element={<MyBooksPage/>}/>
                         <Route path="/profile" element={<ProfilePage/>}/>
                         <Route path="*" element={<Navigate to="/" replace/>}/>
+                        <Route path="/book" element={<BookPage/>}/>
                     </Routes>
                 </main>
             </div>
