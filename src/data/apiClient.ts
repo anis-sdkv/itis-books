75630a4ae6edf97ab5ecec79e35eeb2b945b92af
Anis <sadikowas@yandex.ru>
2025-04-26 23:27:34 +0300

    auth context added

---
+++ src/data/apiClient.ts
@@ -0,0 +1,9 @@
+﻿import axios from 'axios';
+import { API_URL } from '@/config';
+
+export const apiClient = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
