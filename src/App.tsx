fde8821253cb8686ba49d4cf815a84f8e8edd2ec
Timur <timurka.askarov@inbox.ru>
2025-04-27 23:14:04 +0300

    main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './App.css'
 
 import LayoutHeader from "@/shared/LayoutHeader";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
-import HomePage from '@/pages/HomePage';
 import MyBooksPage from "@/pages/MyBooksPage";
 import ProfilePage from "@/pages/ProfilePage";
 import SearchPage from './pages/SearchPage';
 import BookPage from "@/pages/BookPage";
 import ErrorPage from '@/pages/ErrorPage';
 import AuthPage from "@/pages/Auth/AuthPage";
+import MainPage from "@/pages/MainPage";
 
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
                 <LayoutHeader/>
                 <main className="flex-1 container mx-auto py-6">
                     <Routes>
-                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/" element={<MainPage/>}/>
                         <Route path="/my-books" element={<MyBooksPage/>}/>
                         <Route path="/profile" element={<ProfilePage/>}/>
                         <Route path="/search" element={<SearchPage />} />
